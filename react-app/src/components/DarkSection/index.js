8e369cea46b0c29122a26ce3d40e017dea12e9ca
Your Name <claudiakosylak@gmail.com>
2023-10-16 17:49:31 -0400

    second section of landing page started

---
+++ react-app/src/components/DarkSection/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./DarkSection.css";
+
+function DarkSection() {
+
+    return (
+        <div className="dark-section-wrapper">
+            {/* <div className="dark-section-inner">
+
+            </div> */}
+        </div>
+    )
+}
+
+export default DarkSection;
--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./LandingPage.css";
 import Intro from "../Intro";
+import DarkSection from "../DarkSection";
 
 function LandingPage() {
 
     return (
         <div className="landing-page-wrapper">
             <Intro />
+            <DarkSection />
         </div>
 
     )
