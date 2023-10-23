028611dfcc643715d55aa57325f217b5a54520bc
Your Name <claudiakosylak@gmail.com>
2023-10-23 18:25:10 -0400

    picture on search form

--- react-app/src/components/SearchApartments/index.js
+++ react-app/src/components/SearchApartments/index.js
@@ -8,31 +8,35 @@ function SearchApartments() {
 
     return (
         <div className="search-apartments-wrapper">
-            <div className="search-apartments-inner">
-                <h3>Search Apartments</h3>
-                <form className="search-apartments-form">
-                    <label>
-                        Move-in Date
-                        <input type="date" id="move-in-date" value={moveInDate} onChange={(e) => setMoveInDate(e.target.value)} className="search-apartments-inputs"></input>
-                    </label>
-                    <label>
-                        Bedrooms
-                        <select value={bedrooms} onChange={(e) => setBedrooms(e.target.value)} className="search-apartments-inputs">
-                            <option value="" disabled>select</option>
-                            <option value="studio">Studio</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                        </select>
-                    </label>
-                    <label>
-                        Max rent
-                        <input type="number" value={maxRent} onChange={(e) => setMaxRent(e.target.value)} className="search-apartments-inputs"></input>
-                    </label>
-                    <button type="submit">Check Availability</button>
-                    <button>View All Floor Plans</button>
-                </form>
+            <img src="https://plus.unsplash.com/premium_photo-1661887292823-f92842e8609d?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+            <div className="dark-cover">
+
+                <div className="search-apartments-inner">
+                    <h3>Search Apartments</h3>
+                    <form className="search-apartments-form">
+                        <label>
+                            Move-in Date
+                            <input type="date" id="move-in-date" value={moveInDate} onChange={(e) => setMoveInDate(e.target.value)} className="search-apartments-inputs"></input>
+                        </label>
+                        <label>
+                            Bedrooms
+                            <select value={bedrooms} onChange={(e) => setBedrooms(e.target.value)} className="search-apartments-inputs">
+                                <option value="" disabled>select</option>
+                                <option value="studio">Studio</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                            </select>
+                        </label>
+                        <label>
+                            Max rent
+                            <input type="number" value={maxRent} onChange={(e) => setMaxRent(e.target.value)} className="search-apartments-inputs"></input>
+                        </label>
+                        <button type="submit">Check Availability</button>
+                        <button>View All Floor Plans</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
