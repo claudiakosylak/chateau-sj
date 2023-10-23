deeb9c155e7b1ec08bf49b4bc6b4d8516b9e97b5
Your Name <claudiakosylak@gmail.com>
2023-10-23 17:33:45 -0400

    middle section

--- react-app/src/components/DarkSection/index.js
+++ react-app/src/components/DarkSection/index.js
@@ -5,9 +5,14 @@ function DarkSection() {
 
     return (
         <div className="dark-section-wrapper">
-            {/* <div className="dark-section-inner">
+            <div className="dark-section-inner">
+                <div className="dark-section-left">
+                    <img src="https://images.unsplash.com/photo-1580041065738-e72023775cdc?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+                </div>
+                <div className="dark-section-right">
 
-            </div> */}
+                </div>
+            </div>
         </div>
     )
 }
