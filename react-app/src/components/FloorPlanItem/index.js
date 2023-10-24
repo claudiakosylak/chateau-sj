1e3e6dcf8868395360737a9df5383d5890e0c66b
Your Name <claudiakosylak@gmail.com>
2023-10-24 14:34:37 -0400

    basic floor plan items created

--- react-app/src/components/FloorPlanItem/index.js
+++ react-app/src/components/FloorPlanItem/index.js
@@ -6,6 +6,17 @@ function FloorPlanItem({apartment}) {
     return (
         <div className="floor-plan-item-wrapper">
             <h4>{apartment.name}</h4>
+            <div className="bed-bath-feet">
+                <p>{apartment.bedrooms !== 0 && apartment.bedrooms}{apartment.bedrooms === 0 ? "STUDIO" : apartment.bedrooms === 1 ? " BED" : " BEDS"}</p>
+                <p>{apartment.bathrooms} {apartment.bathrooms === 1 ? "BATH" : "BATHS"}</p>
+                <p>{apartment.squareFeet} SQ. FT.</p>
+            </div>
+            <img src="https://images.unsplash.com/photo-1582239052618-4e2324cef034?auto=format&fit=crop&q=80&w=2874&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+            <div className="rent-deposit">
+                <p>Starting at ${apartment.monthlyRent}</p>
+                <p>Deposit ${apartment.deposit}</p>
+            </div>
+            <button className="availability-button">Availability</button>
         </div>
     )
 }
--- react-app/src/mock-data.js
+++ react-app/src/mock-data.js
@@ -1,4 +1,3 @@
-
 export const apartmentUnits = [
     {
       name: "Plan A",
