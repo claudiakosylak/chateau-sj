5693b965db675fc2670b2c80d6125584e29762f8
Your Name <claudiakosylak@gmail.com>
2023-10-25 19:46:50 -0400

    made apartment model and migration adjustment

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
+from .apartment import Apartment
 from .db import environment, SCHEMA
---
+++ app/models/apartment.py
@@ -0,0 +1,32 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Apartment(db.Model):
+    __tablename__ = "apartment"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    floor_plan = db.Column(db.Integer, nullable=False)
+    bedrooms = db.Column(db.Integer, nullable=False)
+    bathrooms = db.Column(db.Integer, nullable=False)
+    square_feet = db.Column(db.Integer, nullable=False)
+    monthly_rent = db.Column(db.Integer, nullable=False)
+    deposit_amount = db.Column(db.Integer, nullable=False)
+    floor_plan_image = db.String(db.String(300))
+    is_available = db.Column(db.Boolean, nullable=False)
+    date_available = db.Column(db.Date, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'floor_plan': self.floor_plan,
+            'bedrooms': self.bedrooms,
+            'bathrooms': self.bathrooms,
+            'square_feet': self.square_feet,
+            'monthly_rent': self.monthly_rent,
+            'deposit_amount': self.deposit_amount,
+            'floor_plan_image': self.floor_plan_image,
+            'is_available': self.is_available,
+            'date_available': self.date_available
+        }
