58160250a233a34f8480b31c380c382b95fd22bd
Your Name <claudiakosylak@gmail.com>
2023-11-08 17:13:00 +0100

    beginning of apartments index

--- react-app/src/components/FloorPlanIndex/index.js
+++ react-app/src/components/FloorPlanIndex/index.js
@@ -1,27 +1,47 @@
 import React, { useEffect } from "react";
 import "./FloorPlanIndex.css";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { useDispatch, useSelector } from "react-redux";
 import { getFloorPlansThunk } from "../../store/floor_plan";
+import { getApartmentsThunk } from "../../store/apartment";
 
 function FloorPlanIndex() {
     const dispatch = useDispatch();
+    const history = useHistory();
     const { id } = useParams();
     const floorPlans = useSelector(state => state.floorPlan.floorPlans);
+    const allApartments = useSelector(state => state.apartment.apartments);
+    const apartmentsList = Object.values(allApartments);
     const plan = floorPlans[id];
+    const apartments = apartmentsList.filter((apartment) => apartment.floor_plan_id === plan.id)
 
     useEffect(() => {
         dispatch(getFloorPlansThunk());
+        dispatch(getApartmentsThunk());
     }, [])
 
-    console.log("FLOOR PLAN: ", plan)
+    console.log("apartments: ", apartments)
 
     return (
         <div className="fpi-wrapper">
-            <h2>
-                {plan?.name}
+            <div className="fpi-top">
+                <i className="fa-solid fa-arrow-left back-button" onClick={() => history.push("/floor-plans")}></i>
+                <h2>{plan?.name}</h2>
+                <div className="fpi-top-middle">
 
-            </h2>
+                    <div className="fpi-carousel">
+                        <img src={plan?.image_1} alt={`${plan?.name} picture`}></img>
+                    </div>
+                    <div className="fpi-apartments-list">
+                        {apartments.map((apartment, index) => (
+                            <div key={apartment.id}>
+                                <p>{index + 1}</p>
+                                <p>{apartment.date_available}</p>
+                                </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
