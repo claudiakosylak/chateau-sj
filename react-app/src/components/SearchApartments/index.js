11b5d963200c9a7684ed269bdf1ce4cc3393aa48
Your Name <claudiakosylak@gmail.com>
2023-11-10 00:24:04 +0100

    styling for landing page search apts

--- react-app/src/components/SearchApartments/index.js
+++ react-app/src/components/SearchApartments/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Redirect, useHistory } from "react-router-dom";
-import "./SearchApartments.css";
+import styles from "./SearchApartments.module.sass";
 import { maxRents } from '../../mock-data';
 
 function SearchApartments({ scrollToRef, setCriteria }) {
@@ -37,27 +37,27 @@ function SearchApartments({ scrollToRef, setCriteria }) {
     }
 
     return (
-        <div className="search-apartments-wrapper">
-            <div className="scroll-apartments-ref" ref={scrollToRef}></div>
+        <div className={styles.wrapper}>
+            <div className={styles.ref} ref={scrollToRef}></div>
             <img src="https://plus.unsplash.com/premium_photo-1661887292823-f92842e8609d?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
-            <div className="dark-cover">
+            <div className={styles.dark}>
 
-                <div className="search-apartments-inner">
+                <div className={styles.inner}>
                     <h3>Search Apartments</h3>
-                    <form className="search-apartments-form" onSubmit={handleSubmit}>
+                    <form className={styles.form} onSubmit={handleSubmit}>
                         {(hasSubmitted && errors.date) && (
-                            <p className="search-errors">{errors.date}</p>
+                            <p className={styles.errors}>{errors.date}</p>
                         )}
                         <label>
                             Move-in Date
-                            <input type="date" id="move-in-date" value={moveInDate} onChange={(e) => setMoveInDate(e.target.value)} className="search-apartments-inputs"></input>
+                            <input type="date" id="move-in-date" value={moveInDate} onChange={(e) => setMoveInDate(e.target.value)} className={styles.inputs}></input>
                         </label>
                         {(hasSubmitted && errors.bedrooms) && (
-                            <p className="search-errors">{errors.bedrooms}</p>
+                            <p className={styles.errors}>{errors.bedrooms}</p>
                         )}
                         <label>
                             Bedrooms
-                            <select value={bedrooms} onChange={(e) => setBedrooms(e.target.value)} className="search-apartments-inputs">
+                            <select value={bedrooms} onChange={(e) => setBedrooms(e.target.value)} className={styles.inputs}>
                                 <option value="" disabled>select</option>
                                 <option value="studio">Studio</option>
                                 <option value="1">1</option>
@@ -67,11 +67,11 @@ function SearchApartments({ scrollToRef, setCriteria }) {
                             </select>
                         </label>
                         {(hasSubmitted && errors.rent) && (
-                            <p className="search-errors">{errors.rent}</p>
+                            <p className={styles.errors}>{errors.rent}</p>
                         )}
                         <label>
                             Max Rent
-                            <select value={maxRent} onChange={(e) => setMaxRent(e.target.value)} className="search-apartments-inputs">
+                            <select value={maxRent} onChange={(e) => setMaxRent(e.target.value)} className={styles.inputs}>
                                 <option value="" disabled>select</option>
                                 {maxRents.map(rent => (
                                     <option value={rent.toString()} key={rent}>${rent}</option>
