d8359d0db79f3b6d05d83ebc7a346e34f14b532e
Your Name <claudiakosylak@gmail.com>
2023-11-14 14:47:12 +0100

    mobile changes for floor plans page

--- react-app/src/components/FloorPlanItem/index.js
+++ react-app/src/components/FloorPlanItem/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./FloorPlanItem.css";
+import styles from "./FloorPlanItem.module.sass";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function FloorPlanItem({floorPlan}) {
@@ -10,19 +10,19 @@ function FloorPlanItem({floorPlan}) {
     }
 
     return (
-        <div className="floor-plan-item-wrapper">
+        <div className={styles.wrapper}>
             <h4>{floorPlan.name}</h4>
-            <div className="bed-bath-feet">
+            <div className={styles.bed_bath_feet}>
                 <p>{floorPlan.bedrooms !== 0 && floorPlan.bedrooms}{floorPlan.bedrooms === 0 ? "STUDIO" : floorPlan.bedrooms === 1 ? " BED" : " BEDS"}</p>
                 <p>{floorPlan.bathrooms} {floorPlan.bathrooms === 1 ? "BATH" : "BATHS"}</p>
                 <p>{floorPlan.square_feet} SQ. FT.</p>
             </div>
             <img src="https://images.unsplash.com/photo-1582239052618-4e2324cef034"></img>
-            <div className="rent-deposit">
+            <div className={styles.rent_deposit}>
                 <p>Starting at ${floorPlan.monthly_rent}</p>
                 <p>Deposit ${floorPlan.deposit_amount}</p>
             </div>
-            <button className="availability-button" onClick={() => openFloorPlan(floorPlan.id)}>Availability</button>
+            <button className={styles.availability} onClick={() => openFloorPlan(floorPlan.id)}>Availability</button>
         </div>
     )
 }
